9815fab912728f9ba09dd4844f84827e4283aeaf
Gardn <gardn769@gmail.com>
2022-08-04 18:42:47 +0300

    +multer

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser')
+
+const error404 = require('./middleware/error-404')
+const booksRouter = require('./routes/api/books')
+const usersRouter = require('./routes/api/users')
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+// app.use('/public', express.static(__dirname+'/public'))
+
+app.use(bodyParser.json())
+app.use('/api/books', booksRouter)
+app.use('/api/users', usersRouter)
+app.use(error404)
+
+const publicPath = path.join('../', 'storage')
+if (!fs.existsSync(publicPath)) {
+  fs.mkdirSync(publicPath)
+}
+
+
+app.listen(PORT, console.log(`App listening on port ${PORT}`));
---
+++ src/routes/api/books.js
@@ -0,0 +1,118 @@
+const Book = require("../../database/entity/book")
+const { books } = require("../../store")
+const path = require('path')
+const express = require('express')
+const router = express.Router()
+const fileMulter = require('../../middleware/filemulter')
+
+
+// .post('/upload-img',
+//     fileMulter.single('cover-img'),
+//     (req, res) => {
+//         if(req.file){
+//             const {path} = req.file
+//             res.json({path})
+//         }
+//         res.json()
+//     })
+router
+.get('/:id/download', (req, res) => {
+    const { id } = req.params
+    const idx = books.findIndex(el => el.id === id)
+
+    if (idx === -1) {
+        res.status(404)
+        res.send('книга не найдена')
+        return
+    }
+
+    const { fileBook } = books[idx]
+    const file = path.join(process.env.APP_ROOT, 'public', 'books', fileBook)
+    res.download(file)
+    })
+
+.get("/", (req, res) => {
+    // const { books } = store;
+    res.json(books);
+  })
+
+.get("/:id", (req, res) => {
+    // const { books } = store;
+    const { id } = req.params;
+    const idx = books.findIndex((el) => el.id === id);
+
+    if (idx == -1) {
+      res.status(404);
+      res.json("404 | книга не найдена");
+      return;
+    };
+
+   res.json(books[idx]);
+  })
+
+.post("/", (req, res) => {
+    // const { books } = store;
+    console.log(req.body);
+    console.log('post');
+    const { title, description, authors, favorite, fileCover, fileName } = req.body;
+
+    const newbook = new Book(
+      title,
+      description,
+      authors,
+      favorite,
+      fileCover,
+      fileName
+    );
+    books.push(newbook);
+
+    console.log(newbook);
+    res.status(201);
+    res.json(newbook);
+  })
+
+.put("/:id", (req, res) => {
+    // const { books } = store;
+    console.log('put');
+    console.log( req.body);
+    const { id } = req.params;
+    const { title, description, authors, favorite, fileCover, fileName } = req.body;
+    const idx = books.findIndex((el) => el.id === id);
+
+    if (idx == -1) {
+      res.status(404);
+      res.json("404 | книга не найдена");
+      return
+    };
+
+      books[idx] = {
+        ...books[idx],
+        title,
+        description,
+        authors,
+        favorite,
+        fileCover,
+        fileName,
+      };
+
+      res.json(books[idx]);
+
+  })
+
+.delete("/:id", (req, res) => {
+    // const { books } = store;
+    const { id } = req.params;
+    const idx = books.findIndex((el) => el.id === id);
+
+    if (idx == -1) {
+      res.status(404);
+      res.json("404 | книга не найдена");
+      return
+    }
+
+    books.splice(idx, 1);
+    res.json("ok");
+
+  });
+
+module.exports = router
