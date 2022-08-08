6a2ac9b2405ffb47c35096d9c6e4b803c5e97923
Gardn <gardn769@gmail.com>
2022-08-08 18:28:09 +0300

    +front from ejs

--- src/index.js
+++ src/index.js
@@ -3,21 +3,29 @@ const path = require('path');
 const fs = require('fs');
 
 const error404 = require('./middleware/error-404')
+const indexRouter = require('./routes/index')
 const booksRouter = require('./routes/api/books')
 const usersRouter = require('./routes/api/users')
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.set("views",  'src/views')
+app.use('/', indexRouter)
 
 app.use('/api/books', booksRouter)
 app.use('/api/users', usersRouter)
-app.use(error404)
 
 const publicPath = path.join('', 'storage')
+app.use('/storage', express.static(publicPath) )
+
 if (!fs.existsSync(publicPath)) {
   fs.mkdirSync(publicPath)
 }
+
+app.use(error404)
 
 
 app.listen(PORT, console.log(`App listening on port ${PORT}`));
